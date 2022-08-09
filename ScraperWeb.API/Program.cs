13939bf4994ec3e7b6819d7af305677646bc0997
Fábio Mattes <fabio.mattes@benner.com.br>
2022-08-09 11:34:23 -0300

    CORS

--- ScraperWeb.API/Program.cs
+++ ScraperWeb.API/Program.cs
@@ -1,11 +1,21 @@
 using ScraperWeb.API.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
 
 var app = builder.Build();
 
@@ -16,6 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 
 app.MapGet("/search", (string assunto) =>
